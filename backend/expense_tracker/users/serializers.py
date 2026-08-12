151523dc830f75ea680a230b6039203c7fd067a0
1-ashish-04 <ashish4.jayaswal@gmail.com>
2026-08-12 23:08:54 +0530

    add email validation

--- backend/expense_tracker/users/serializers.py
+++ backend/expense_tracker/users/serializers.py
@@ -3,7 +3,15 @@ from .models import User
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8)
+
+    password = serializers.CharField(
+        write_only=True,
+        min_length=8
+    )
+
+    confirm_password = serializers.CharField(
+        write_only=True
+    )
 
     class Meta:
         model = User
@@ -12,9 +20,33 @@ class RegisterSerializer(serializers.ModelSerializer):
             "username",
             "email",
             "password",
+            "confirm_password",
         ]
 
+    def validate(self, attrs):
+
+        if attrs["password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError(
+                {
+                    "confirm_password":
+                    "Passwords do not match."
+                }
+            )
+
+        return attrs
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError(
+                "Email already exists."
+            )
+
+        return value
+
     def create(self, validated_data):
+
+        validated_data.pop("confirm_password")
+
         return User.objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
